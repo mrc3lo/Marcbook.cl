80d860187fb6526c5817c0361ec96078dbedb4f2
Marcelo Diaz Flores <marcelo.diaz91@inacapmail.cl>
2026-05-26 19:57:17 -0400

    Pimer commit Marcbook

---
+++ script.js
@@ -0,0 +1,18 @@
+// ==========================
+// NAVBAR INTELIGENTE
+// ==========================
+
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 40){
+
+        header.classList.add("scrolled");
+
+    }else{
+
+        header.classList.remove("scrolled");
+    }
+
+});
