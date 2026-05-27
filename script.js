57805b0e6386d33ba05e6b8c4e1ae2766a3bef03
Marcelo Diaz Flores <marcelo.diaz91@inacapmail.cl>
2026-05-26 23:09:25 -0400

    tercer commit

--- script.js
+++ script.js
@@ -16,3 +16,28 @@ window.addEventListener("scroll", () => {
     }
 
 });
+
+const sliders = document.querySelectorAll(".slider-servicio");
+
+sliders.forEach((slider) => {
+
+    const slides = slider.querySelectorAll(".slide");
+
+    let index = 0;
+
+    setInterval(() => {
+
+        slides[index].classList.remove("active");
+
+        index++;
+
+        if(index >= slides.length){
+
+            index = 0;
+        }
+
+        slides[index].classList.add("active");
+
+    }, 3500);
+
+});
